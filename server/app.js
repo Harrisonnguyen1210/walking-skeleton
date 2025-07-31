47901a4ce93e38d5120ba5eaf1e5de9faab8769b
Hung Nguyen <giahungftu2@gmail.com>
2025-07-31 22:33:06 +0300

    Lazy loading

--- server/app.js
+++ server/app.js
@@ -57,21 +57,36 @@ app.get("/hybrid", async (c) => {
   return c.html(`<html>
     <head>
       <script>
-        document.addEventListener("DOMContentLoaded", async () => {
+        const loadRemaining = async () => {
           const list = document.getElementById("list");
           const items = await fetch("http://localhost:8000/items/remaining");
           const json = await items.json();
+
+          document.getElementById("last")?.remove();
+
           for (const item of json) {
             const li = document.createElement("li");
             li.textContent = item.name;
             list.appendChild(li);
           }
+        };
+
+        document.addEventListener("DOMContentLoaded", () => {
+          const observer = new IntersectionObserver((entries, obs) => {
+            if (entries[0].isIntersecting) {
+              loadRemaining();
+              obs.disconnect();
+            }
+          });
+
+          observer.observe(document.getElementById('last'));
         });
       </script>
     </head>
     <body>
       <ul id="list">
         ${items.map((item) => `<li>${item.name}</li>`).join("")}
+        <li id="last">Loading...</li>
       </ul>
     </body>
   </html>`);
