bba76b5efb011c596006cdd1507d72f92b5b91e0
Hung Nguyen <giahungftu2@gmail.com>
2025-08-01 23:08:43 +0300

    Add svelte

---
+++ client/src/components/InputStatistics.svelte
@@ -0,0 +1,30 @@
+<!-- implement functionality for input statistics here -->
+<script>
+    let inputValue = "";
+    let numbers = [];
+    let sum = 0;
+    let count = 0;
+
+    const updateStats = () => {
+        sum = numbers.reduce((a, b) => a + b, 0);
+        count = numbers.length;
+    };
+
+    const addNumber = () => {
+        numbers = [...numbers, parseFloat(inputValue)];
+        updateStats();
+        inputValue = "";
+    };
+</script>
+
+<input type="number" bind:value={inputValue} />
+<button on:click={addNumber}>Add</button>
+
+<ul>
+    {#each numbers as num}
+        <li>{num}</li>
+    {/each}
+</ul>
+
+<p>Sum: {sum}</p>
+<p>Total numbers: {count}</p>
